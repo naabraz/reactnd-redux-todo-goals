a66c5150659b718688be61b2199b67dd1fb1a154
Natalia Braz <naabraz@gmail.com>
2018-09-20 12:38:15 -0300

    adding dispatch calling examples

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const ADD_TODO = 'ADD_TODO'
-const REMOVE_TODO = 'REMOVE_TODO'
-const TOGGLE_TODO = 'TOGGLE_TODO'
-const ADD_GOAL = 'ADD_GOAL'
-const REMOVE_GOAL = 'REMOVE_GOAL'
-
 //Library code
 function createStore (reducer) {
   // The store should have four parts
@@ -25,7 +19,7 @@ function createStore (reducer) {
     }
   }
 
-  //state will be whatever reducer (todos) function returns
+  //state will be whatever reducer (app {todos, goals}) function returns
   const dispatch = (action) => {
     state = reducer(state, action)
     listeners.forEach((listener) => listener()) //calls the function passed to subscribe function
@@ -39,21 +33,28 @@ function createStore (reducer) {
 }
 
 //App code
+const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+const ADD_GOAL = 'ADD_GOAL'
+const REMOVE_GOAL = 'REMOVE_GOAL'
+
 //this is a reducer and must be a pure function
 function todos (state = [], action) {
   switch (action.type) {
-    case ADD_TODO :
+    case ADD_TODO:
       return state.concat([action.todo])
-    case REMOVE_TODO :
+    case REMOVE_TODO:
       return state.filter((todo) => todo.id !== action.id)
-    case TOGGLE_TODO :
+    case TOGGLE_TODO:
       return state.map((todo) => todo.id !== action.id ? todo :
         Object.assign({}, todo, { complete: !todo.complete }))
-    default :
+    default:
       return state
   }
 }
 
+//this is another reducer
 function goals (state = [], action) {
   switch(action.type) {
     case ADD_GOAL :
@@ -65,7 +66,7 @@ function goals (state = [], action) {
   }
 }
 
-//root reducer
+//root reducer (combining the two reducers)
 const app = (state = {}, action) => {
   return {
     todos: todos(state.todos, action),
@@ -77,4 +78,23 @@ const store = createStore(app)
 
 store.subscribe(() => {
   console.log('The new state is: ', store.getState())
+})
+
+store.dispatch({
+  type: ADD_TODO,
+  todo: {
+    id: 0,
+    name: 'Walk the dog',
+    complete: false,
+  }
+})
+
+store.dispatch({
+  type: REMOVE_TODO,
+  id: 1
+})
+
+store.dispatch({
+  type: TOGGLE_TODO,
+  id: 0
 })
