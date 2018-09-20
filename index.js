65b86e41d861d68b71c573ed796aac3b3931c774
Natalia Braz <naabraz@gmail.com>
2018-09-19 21:17:02 -0300

    adding goals

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+const ADD_GOAL = 'ADD_GOAL'
+const REMOVE_GOAL = 'REMOVE_GOAL'
 
 //Library code
 function createStore (reducer) {
@@ -38,11 +42,39 @@ function createStore (reducer) {
 //this is a reducer and must be a pure function
 function todos (state = [], action) {
   switch (action.type) {
-    case ADD_TODO:
+    case ADD_TODO :
       return state.concat([action.todo])
-    default:
+    case REMOVE_TODO :
+      return state.filter((todo) => todo.id !== action.id)
+    case TOGGLE_TODO :
+      return state.map((todo) => todo.id !== action.id ? todo :
+        Object.assign({}, todo, { complete: !todo.complete }))
+    default :
       return state
   }
 }
 
-const store = createStore(todos)
+function goals (state = [], action) {
+  switch(action.type) {
+    case ADD_GOAL :
+      return state.concat([action.goal])
+    case REMOVE_GOAL :
+      return state.filter((goal) => goal.id !== action.id)
+    default :
+      return state
+  }
+}
+
+//root reducer
+const app = (state = {}, action) => {
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action),
+  }
+}
+
+const store = createStore(app)
+
+store.subscribe(() => {
+  console.log('The new state is: ', store.getState())
+})
